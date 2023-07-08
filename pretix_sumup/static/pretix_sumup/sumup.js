0247deb73710ebf8cacf5c90a62c4959a3f05621
Ronan LE MEILLAT <eltorio@users.noreply.github.com>
2023-07-08 10:19:14 +0200

    wip

--- pretix_sumup/static/pretix_sumup/sumup.js
+++ pretix_sumup/static/pretix_sumup/sumup.js
@@ -18,10 +18,30 @@ function waitForElm(selector) {
         });
     });
 }
+function radioClickHandler() {
+    let submitElm = document.querySelector('button[type="submit"]');
+    if(document.querySelector("#input_payment_sumuppayment").checked){
+        submitElm.classList.add('hidden');
+        SumUpCard.mount({
+            id: 'sumup-card',
+            checkoutId: '2ceffb63-cbbe-4227-87cf-0409dd191a98',
+            onResponse: function (type, body) {
+              console.log('Type', type);
+              console.log('Body', body);
+            },
+          });
+    }else{
+        submitElm.classList.remove('hidden');
+    }
+}
 
-waitForElm("#input_payment_sumuppayment").then((elm) => {
+waitForElm("#input_payment_sumuppayment").then((sumupElm) => {
     console.log('Element #input_payment_sumuppayment is ready');
-    elm.addEventListener("click", function(){
-        alert("click");
-      });
+    waitForElm('button[type="submit"]').then((submitElm) => {
+        console.log('Submit is ready');
+        document.querySelectorAll('input[type="radio"]').forEach((radioElm) => {
+            console.log(`add listener for ${radioElm}`);
+            radioElm.parentElement.addEventListener("click", radioClickHandler);
+        })
+    })
 });
